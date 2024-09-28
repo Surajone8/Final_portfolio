518eef48c625bc6300e57bcb9ca4787e8c1ab974
SurajOne8 <ysuraj18333@gmail.com>
2024-09-28 13:30:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BackgroundVideo from './components/BackgroundVideo';
+
+function App() {
+  return (
+    <>
+        <BackgroundVideo />
+    </>
+  );
+}
+
+export default App;
